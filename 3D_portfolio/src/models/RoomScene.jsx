fab4b2f3fb7717e24bf0a830168f23920aa0b4c3
Timeshot0311 <suhil0311@gmail.com>
2025-08-23 03:16:27 +0200

    Create timeshot-room2.glb

--- 3D_portfolio/src/models/RoomScene.jsx
+++ 3D_portfolio/src/models/RoomScene.jsx
@@ -1,9 +1,8 @@
 import { useGLTF } from '@react-three/drei';
 
 export default function RoomScene() {
-  const { scene } = useGLTF('/models/timeshot-room.glb');
-
-  
+  const { scene } = useGLTF('/models/timeshot-room2.glb');
+  console.log("GLB loaded:", scene); // ✅ now inside the component
 
   return <primitive object={scene} />;
 }
